4fe868996cf175c746b15b21097455636dcf1aa7
mcstewart76 <mcstewart76@gmail.com>
2022-01-15 16:03:35 -0500

    Reformatted my index to be broken up into lib folder, added a bootstrap index and css for testing

--- index.js
+++ index.js
@@ -1,72 +1,16 @@
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
 var inquirer = require("inquirer");
+
+
+
+
 //var fs = require("fs");
 
 
-class Employee{
-    constructor(name, id, email){
-        this.name = name;
-        this.id = id;
-        this.email = email;
-    }
-    getName(){
-    const employeeName = new Employee(this.name)
-    console.log(this.name)
-    console.log(employeeName.name)
-    };
-
-    getId(){
-    const employeeName = new Employee("test", this.id)
-    console.log(this.id)
-    console.log(employeeName.id)
-    }
-
-    getEmail(){
-    const employeeName = new Employee("test", 0, this.email)
-    console.log(this.email)
-    console.log(employeeName.email)
-    }
-    getRole(){
-        const employeeName = new Employee();
-        console.log(employeeName.constructor.name)
-        return employeeName.constructor.name
-
-    };
-}
-
-
-
-
-class Manager extends Employee{
-    constructor(name, id, email, officeNum){
-        super(name, id, email);
-            this.officeNum = officeNum;
-
-    }
-    getRole(){
-        const employeeName = new Manager();
-        console.log(employeeName.constructor.name)
-        return employeeName.constructor.name
-    }
-}
-class Engineer extends Employee{
-    constructor(name, id, email, github){
-        super(name, id, email);
-            this.github = github;
-
-    }
-    getGitHub(){
-        const engineer = new Engineer(this.github)
-        console.log(this.github)
-        console.log(engineer.github)
-        
-
-    }
-    getRole(){
-        const employeeName = new Engineer();
-        console.log(employeeName.constructor.name)
-        return employeeName.constructor.name
-    }
-}
 // const e = new Engineer("jim", 47, "test@test.com");
 // e.getName();
 // e.getId();
@@ -78,25 +22,9 @@ class Engineer extends Employee{
 // ef.getEmail();
 // ef.getRole();
 
-class Intern extends Employee{
-    constructor(name, id, email, school){
-        super(name, id, email);
-            this.school = school;
 
-    }
-    getSchool(){
-        const intern = new Intern(this.school)
-        console.log(this.school)
-        console.log(intern.school)
-    }
-    getRole(){
-        const employeeName = new Intern();
-        console.log(employeeName.constructor.name)
-        return employeeName.constructor.name
-    }
-}
 
-const questions = [
+const manQuestions = [
     {
         type: 'input',
         name: 'teammanager',
@@ -146,33 +74,50 @@ const questions = [
             }
         }
     },
+    {
+        type: 'list',
+        name: 'addEmployee',
+        message: 'Select one of the options below.',
+        choices: ['Add Engineer', 'Add Intern', 'Finished building team'],
+        
+    },
     
 
 ];
-inquirer
-  .prompt(questions)
-  .then((data) => {
-      console.log(data);
-       return workers(data);
-      //console.log(data.projectName)
-      // const dataset = (JSON.stringify(answers, null, '  '));
-      // console.log(dataset);
-      // console.log(dataset);
-      
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      return "Prompt couldn't be rendered in the current environment"
-    } else {
-      return "Something else went wrong"
-    }
-  });
-const workers = data => {
-    const jim = new Manager(data.teammanager, data.managerID, data.managerEmail, data.officeNum);
 
-    console.log(jim);
-    const e = new Engineer()
+// const workers = data => {
+//     const theManager = new Manager(data.teammanager, data.managerID, data.managerEmail, data.managerOfficeNum);
+
+// }
+let team = [];
+
+function askQuestions(){
+    inquirer
+    .prompt(manQuestions)
+    .then((data) => {
+        console.log(data);
+        const theManager = new Manager(data.teammanager, data.managerID, data.managerEmail, data.managerOfficeNum);
+        console.log(theManager.getRole());
+        team.push(theManager);
+        console.log(`team = ` + team[0])
+        return theManager;
+        //console.log(data.projectName)
+        // const dataset = (JSON.stringify(answers, null, '  '));
+        // console.log(dataset);
+        // console.log(dataset);
+        
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+        return "Prompt couldn't be rendered in the current environment"
+        } else {
+        return "Something else went wrong"
+        }
+    });
 }
+askQuestions(); 
+
+
 
 // const employees = workers.map()
 // console.log(employees);
