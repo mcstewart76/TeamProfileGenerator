02e1e6e4721e531475013459d92799bebe33f335
mcstewart76 <mcstewart76@gmail.com>
2022-01-15 21:19:16 -0500

    Removed lots of comments, gave each js file their own card

--- index.js
+++ index.js
@@ -5,28 +5,11 @@ const Intern = require("./lib/Intern");
 
 var inquirer = require("inquirer");
 
-intern = new Intern;
-engineer = new Engineer;
-employee = new Employee;
-
-
 var fs = require("fs");
-
 
 const managers = [];
 const engineers = [];
 const interns = [];
-// const e = new Engineer("jim", 47, "test@test.com");
-// e.getName();
-// e.getId();
-// e.getEmail();
-// e.getRole();
-// const ef = new Employee("jim", 47, "test@test.com");
-// ef.getName();
-// ef.getId();
-// ef.getEmail();
-// ef.getRole();
-
 
 
 const manQuestions = [
@@ -83,6 +66,7 @@ const manQuestions = [
     
 
 ];
+
 const moreMembers = [{
     type: 'list',
     name: 'addEmployee',
@@ -90,6 +74,7 @@ const moreMembers = [{
     choices: ['Add Engineer', 'Add Intern', 'Finished building team'],
     
 }];
+
 const engQuestions = [
     {
         type: 'input',
@@ -193,40 +178,7 @@ const intQuestions = [
         }
     },
 ]
-// const workers = data => {
-//     const theManager = new Manager(data.teammanager, data.managerID, data.managerEmail, data.managerOfficeNum);
 
-// }
-
-
-// function askQuestions(){
-//     return new Promise((resolve, reject) => {
-//     inquirer
-//     .prompt(manQuestions)
-//     .then((data) => {
-//         console.log(data);
-//         const theManager = new Manager(data.teammanager, data.managerID, data.managerEmail, data.managerOfficeNum);
-//         console.log(theManager.getRole());
-//         team.push(theManager);
-//        // console.log(`team = ` + team[0])
-//         return data
-        
-//         //console.log(data.projectName)
-//         // const dataset = (JSON.stringify(answers, null, '  '));
-//         // console.log(dataset);
-//         // console.log(dataset);
-        
-//     })
-//     .catch((error) => {
-//         if (error.isTtyError) {
-//         return "Prompt couldn't be rendered in the current environment"
-//         } else {
-//         return "Something else went wrong"
-//         }
-//     });
-//     }
-    
-//     )};
 function askMoreMembers2() {
     
      return   inquirer
@@ -260,24 +212,7 @@ function askQuestions() {
         
     };
 
-// function askMoreMembers(){
-//     inquirer
-//     .prompt(moreMembers)
-//     .then((ismore) => {
-        
-        
-//        // console.log(`team = ` + team[0])
-//         return ismore
-        
-//     })
-//     .catch((error) => {
-//         if (error.isTtyError) {
-//         return "Prompt couldn't be rendered in the current environment"
-//         } else {
-//         return "Something else went wrong"
-//         }
-//     });
-// }
+
 function askEngineer(){
     
     return   inquirer
@@ -335,48 +270,11 @@ function htmlgen (){
     <nav class="navbar navbar-dark bg-dark">
         <span class="navbar-brand mb-0 h1 ">Team Profile Generator</span>
       </nav>
-    <div class="container mx-auto mt-4">
-        <div class="row">
-            <div class="col-md-4">
-                <div class="card" style="width: 18rem;">
-                    
-                    <div class="card-body">
-                        <h5 class="card-title"></h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                            card's content.</p>
-                        <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Visit Site</a>
-                        <a href="#" class="btn "><i class="fab fa-github"></i> Github</a>
-                    </div>
-                </div>
-            </div>
-            
-            
-    
-            
-           
-        </div>
-    </div>
+      <div class="newCard">
+      </div>
     
 </body>
 </html>`
-};
-function htmlcard() {
-const card = `
-<div class="row">
-<div class="col-md-4">
-    <div class="card" style="width: 18rem;">
-        
-        <div class="card-body">
-            <h5 class="card-title"></h5>
-            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                card's content.</p>
-            <a href="#" class="btn mr-2"><i class="fas fa-link"></i> Visit Site</a>
-            <a href="#" class="btn "><i class="fab fa-github"></i> Github</a>
-        </div>
-    </div>
-</div>`    
 };
 
 const runner = async () => {
@@ -393,18 +291,6 @@ runner();
 
 
 
-
-
-
-
-
-
-// // const employees = workers.map()
-// // console.log(employees);
-
-// // employees.forEach(employee => {
-// // console.log(employee);
-// // });
 
 
 
