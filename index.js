0c4aa951cd6648046d1b51963efdfa3174ac1afc
mcstewart76 <mcstewart76@gmail.com>
2022-01-16 00:32:51 -0500

    Did a hotfix to just use string concatination to build the site. Removed lots of unnecessary code, still needs refactoring

--- index.js
+++ index.js
@@ -5,17 +5,36 @@ const Intern = require("./lib/Intern");
 
 var inquirer = require("inquirer");
 
-var fs = require("fs");
+var fs = require('fs');
 
-const managers = [];
-const engineers = [];
-const interns = [];
 
+;
+const htmlstring = [`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    
+    <link href="css/styles.css" rel="stylesheet" />
+    <script src="index.js" defer></script>
+
+    <title>Team Profile Generator</title>
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <span class="navbar-brand mb-0 h1 ">Team Profile Generator</span>
+      </nav>
+<div id="newCard">`]
 
 const manQuestions = [
     {
         type: 'input',
-        name: 'teammanager',
+        name: 'teamManager',
         message: 'What is the team managers name?',
         validate: (value) => {
             if(value){
@@ -52,7 +71,7 @@ const manQuestions = [
     },
     {
         type: 'input',
-        name: 'managerOfficeNum',
+        name: 'managerOfficeNumber',
         message: 'What is the team managers office number?',
         validate: (value) => {
             if(value){
@@ -130,7 +149,7 @@ const engQuestions = [
 const intQuestions = [
     {
         type: 'input',
-        name: 'InternName',
+        name: 'internName',
         message: 'What is the Interns name?',
         validate: (value) => {
             if(value){
@@ -179,7 +198,7 @@ const intQuestions = [
     },
 ]
 
-function askMoreMembers2() {
+function askMoreMembers() {
     
      return   inquirer
         .prompt(moreMembers)
@@ -192,8 +211,8 @@ function askMoreMembers2() {
             }if(responseForMore.addEmployee == "Add Intern"){
                 askIntern();
             }if(responseForMore.addEmployee == "Finished building team"){
-                console.log("creating HTML")
-               // docCreate();
+                console.log("HTML has been generated @ index.html under dist")
+                docCreate("index.html", htmlstring.join() );
             }
     })
             
@@ -204,12 +223,33 @@ function askQuestions() {
      return   inquirer
         .prompt(manQuestions)
         .then((data) => {
-        return data
+            const nameOfManager = new Manager(data.teamManager, data.managerID, data.managerEmail, data.managerOfficeNumber)
+            
+                const manCard = `
+                <div class="container mx-auto mt-4">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="card" style="width: 18rem;">
+                                
+                                <div class="card-body">
+                                    <h5 class="card-title">${nameOfManager.constructor.name}</h5>
+                                    <h5 class="card-title">${nameOfManager.name}</h5>
+                                    <h6 class="card-text">Employee ID: ${nameOfManager.id}</h6>
+                                    <h6 class="card-text">Office Number: ${nameOfManager.officeNumber}</h6>
+                                    <a href="mailto: ${nameOfManager.email}" class="btn mr-2"><i class="fas fa-link"></i>Email: ${nameOfManager.email}</a>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>`  
+              htmlstring.push(manCard);
+              //console.log(htmlstring)
+              return htmlstring
+            
+       
         
     })
-            
-            
-        
     };
 
 
@@ -220,68 +260,71 @@ function askEngineer(){
         .then((engdata) => {
             
             const nameOfEngineer = new Engineer(engdata.engineerName, engdata.engineerID, engdata.engineerEmail, engdata.engineerGithub)
-             
-            engineers.push(nameOfEngineer);
-            console.log(engineers)
-            console.log("engineer added")
-            askMoreMembers2();
-        return engineers
+            const engCard = `
+    <div class="container mx-auto mt-4">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    
+                    <div class="card-body">
+                        <h5 class="card-title">${nameOfEngineer.constructor.name}</h5>
+                        <h5 class="card-title">${nameOfEngineer.name}</h5>
+                        <p class="card-text">Employee ID: ${nameOfEngineer.id}</p>
+                        <a href="mailto: ${nameOfEngineer.email}" class="btn mr-2"><i class="fas fa-link"></i>Email: ${nameOfEngineer.email}</a>
+                        <a href="https://github.com/${nameOfEngineer.github}" class="btn "><i class="fab fa-github"></i>GitHub: ${nameOfEngineer.github}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`  
+  htmlstring.push(engCard); 
+  console.log("Engineer added!")
+            askMoreMembers();
+        return htmlstring
     
     
     });
 }
 function askIntern(){
-    console.log("intern added")
+    
     return   inquirer
         .prompt(intQuestions)
         .then((intdata) => {
-            const intern = new Intern(intdata.internname, intdata.internID, intdata.internEmail, intdata.internSchool )
-            interns.push(intern);
-            console.log(interns)
-            askMoreMembers2();
+            const intern = new Intern(intdata.internName, intdata.internID, intdata.internEmail, intdata.internSchool )
+           
+                const intCard = `
+                <div class="container mx-auto mt-4">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="card" style="width: 18rem;">
+                                
+                                <div class="card-body">
+                                    <h5 class="card-title">${intern.constructor.name}</h5>
+                                    <h5 class="card-title">${intern.name}</h5>
+                                    <p class="card-text">Employee ID: ${intern.id}</p>
+                                    <p class="card-text">School: ${intern.school}</p>
+                                    <a href="mailto: ${intern.email}" class="btn mr-2"><i class="fas fa-link"></i> ${intern.email}</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>`  
+              htmlstring.push(intCard);
+              console.log("Intern added!")
+            askMoreMembers();
     return "yay Intern"
         });
 }
-function docCreate(){
-    console.log("html makin")
-    return "doc in progress"
-}
 
-    
-function htmlgen (){
 
- `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <link href="css/styles.css" rel="stylesheet" />
-    <script src="index.js" defer></script>
-
-    <title>Team Profile Generator</title>
-</head>
-<body>
-    <nav class="navbar navbar-dark bg-dark">
-        <span class="navbar-brand mb-0 h1 ">Team Profile Generator</span>
-      </nav>
-      <div class="newCard">
-      </div>
-    
-</body>
-</html>`
-};
 
 const runner = async () => {
-const askQ = await askQuestions();
-managers.push(askQ)
-// console.log(askQ)
-const moreQ = await askMoreMembers2();
+const managerInfo = await askQuestions();
+//managercard(askQ);
+console.log("Manager has been added!")
+
+const moreQ = await askMoreMembers();
+const docmake = await docCreate();
 // console.log(moreQ)
 
 // await htmlgen();
@@ -289,7 +332,25 @@ const moreQ = await askMoreMembers2();
 }
 runner();
 
+function docCreate(filename, link){
+    return new Promise((resolve, reject) => {
 
+        fs.writeFile(`./dist/${filename}`,
+         link + `</div> 
+        <script src="../index.js"></script>   
+        </body>
+        </html>`,
+        
+        function (err) {
+          if (err) reject(err);
+          else resolve()
+  
+        })
+  
+      })
+    
+    
+}
 
 
 
